2716b3c6100aad432b836b7ed210c736a0378c18
jiankun001@e.ntu.edu.sg <jiankun001@e.ntu.edu.sg>
2023-09-05 18:22:55 +0800

    Update __init__ with pdae encoders

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,7 @@
 from typing import Union
 from .unet import BeatGANsUNetModel, BeatGANsUNetConfig
 from .unet_autoenc import BeatGANsAutoencConfig, BeatGANsAutoencModel
+from .pdae_encoder import FFHQ128Encoder, CELEBA64Encoder
 
 Model = Union[BeatGANsUNetModel, BeatGANsAutoencModel]
 ModelConfig = Union[BeatGANsUNetConfig, BeatGANsAutoencConfig]
